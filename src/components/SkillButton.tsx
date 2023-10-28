134a583d8c9c9b70f1f9a7294e749f547c3695f3
mary-metilda <mary.metilda@vonnue.com>
2023-10-28 09:48:57 +0530

    fix: increase the size of the icons

--- src/components/SkillButton.tsx
+++ src/components/SkillButton.tsx
@@ -14,7 +14,7 @@ function SkillButton({ darkLines, skill, skillLogo }: SkillButtonProps) {
     <div className="bg-linear-green group-hover:bg-linear-blue-vertical-middle shadow-2xl rounded-full h-15 w-full flex items-center justify-between gap-4 px-4">
       <div className="flex items-center gap-4 text-green-950 text-xl font-semibold">
         <div className="bg-linear-blue-white p-2 rounded-full w-fit h-fit">
-          <img className="w-10 h-10 rounded-full" src={skillLogo} alt="html" />
+          <img className="w-20 h-20 rounded-full" src={skillLogo} alt="html" />
         </div>
         <p>{skill}</p>
       </div>
