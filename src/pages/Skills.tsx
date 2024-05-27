a156084d9b172794d3c51ec6f111ff336d9ca177
mary-metilda <mary.metilda@vonnue.com>
2024-05-27 17:28:19 +0530

    fix: remove gradient

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -116,7 +116,7 @@ function Sample() {
           dotSize={2}
         />
       </AnimatePresence>
-      <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" />
+      {/* <div className="absolute inset-0 [mask-image:radial-gradient(400px_at_center,white,transparent)] bg-black/50 dark:bg-black/90" /> */}
     </div>
   );
 }
