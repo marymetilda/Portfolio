4bebfccd02c347183cc42356ae8d134fc4e2de4f
mary-metilda <mary.metilda@vonnue.com>
2024-05-30 14:27:05 +0530

    feat: add intro page

--- src/components/FlipWords.tsx
+++ src/components/FlipWords.tsx
@@ -1,4 +1,3 @@
-"use client";
 import React, { useCallback, useEffect, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { cn } from "../utils/cn";
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,21 @@ import ContactIcon from "../assets/contact.png";
 import GithubIcon from "../assets/github.png";
 import ResumeIcon from "../assets/resume.png";
 import LinkedIn from "../assets/linkedin.svg";
-import { useEffect, useState } from "react";
 import { FlipWords } from "../components/FlipWords";
 
 const homepageData = Data.home;
 
-const words = ["Mary Metilda", "A Frontend Developer", "A React Developer"];
+const words = [
+  "Mary Metilda",
+  "A Code Connoisseur",
+  "A Tech Enthusiastic",
+  "A UX Maestro",
+  "A Design Aficionado",
+  "A Pixel Perfectionist",
+  "A Perpetual Learner",
+];
 
 function Home() {
-  const [shouldShowName, setShouldShowName] = useState(true);
   const navigate = useNavigate();
 
   const handleAbout = () => {
@@ -33,13 +39,6 @@ function Home() {
     navigate("/contact");
   };
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setShouldShowName(false);
-    }, 3000);
-    return () => clearTimeout(timeout);
-  }, []);
-
   const GradientBar = () => (
     <div className="relative z-10 h-12 w-4 block">
       <div className="absolute inset-y-2 inset-x-2 bg-linear-indigo h-3/4 w-[2px] blur-md" />
@@ -51,202 +50,149 @@ function Home() {
 
   return (
     <div className="w-full h-screen bg-black flex overflow-hidden rounded-md">
-      {shouldShowName ? (
-        <div className="flex flex-col items-center justify-center h-full w-full animate-appear">
-          <div className="flex flex-col items-center justify-center">
-            <p className="text-indigo-400 text-xl animate-appear pb-4">
-              {homepageData.introStatement}
-            </p>
-            <div className="relative flex items-center justify-center">
-              <h1 className="md:text-6xl text-3xl font-bold text-center text-transparent z-20 bg-clip-text bg-indigo-600 blur-md animate-appear">
-                {homepageData.name}
-              </h1>
-              <h1 className="absolute inset-0 md:text-6xl text-3xl font-bold text-center text-transparent z-20 bg-clip-text bg-slate-600 blur-sm animate-appear">
-                {homepageData.name}
-              </h1>
-              <h1 className="absolute inset-0 md:text-6xl text-3xl font-bold text-center text-transparent z-20 bg-clip-text bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-slate-500 via-teal-500 to-slate-500 animate-appear">
-                {homepageData.name}
-              </h1>
-            </div>
-            <p className="text-sky-500 text-[3vw] md:text-xl font-semibold animate-appear pt-4">
-              {homepageData.designation}
-            </p>
+      <div className="flex flex-col items-center justify-start h-full w-full animate-appear">
+        <div className="w-[40rem] h-20 relative mt-4">
+          <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+          <SparklesCore
+            id="particle-2"
+            background="transparent"
+            minSize={0.4}
+            maxSize={1}
+            particleDensity={1200}
+            className="w-full h-full"
+            particleColor="#FFFFFF"
+          />
+          <div className="absolute inset-x-20 bottom-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+          <div className="absolute inset-x-20 bottom-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+          <div className="absolute inset-x-60 bottom-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+          <div className="absolute inset-x-60 bottom-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <br />
+          <div className="text-[3vw] md:text-xl text-sky-600 font-semibold h-fit flex justify-center items-center w-full">
+            <p> I Am </p>
+            <FlipWords words={words} />
           </div>
-          <div className="w-[40rem] h-40 relative mt-4">
-            {/* Gradients */}
-            <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
-            <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
-            <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
-            <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
 
-            {/* Core component */}
-            <SparklesCore
-              id="particle-1"
-              background="transparent"
-              minSize={0.4}
-              maxSize={1}
-              particleDensity={1200}
-              className="w-full h-full"
-              particleColor="#FFFFFF"
-            />
-
-            {/* Radial Gradient to prevent sharp edges */}
-            <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
+          <div className="flex flex-col items-end md:gap-0 pr-0 md:pr-[4vw]">
+            <div className="flex items-end justify-end gap-4 animate-appear pt-8">
+              <Button
+                handleButtonClick={handleAbout}
+                buttonLabel={homepageData.aboutButtonLabel}
+              />
+              <div className="flex flex-col items-center justify-between">
+                <button
+                  onClick={handleAbout}
+                  className="w-8 h-8 p-1 bg-linear-blue rounded-full flex items-center justify-center"
+                >
+                  <img
+                    className="h-full w-full"
+                    src={AboutButton}
+                    alt="about"
+                  />
+                </button>
+              </div>
+            </div>
+            <div className="flex items-end justify-end gap-4 animate-appear">
+              <Button
+                handleButtonClick={handleSkill}
+                buttonLabel={homepageData.skillsButtonLabel}
+              />
+              <div className="flex flex-col items-center">
+                <GradientBar />
+                <button
+                  onClick={handleSkill}
+                  className="w-8 h-8 p-1 bg-linear-blue rounded-full flex items-center justify-center"
+                >
+                  <img
+                    className="h-full w-full rounded-full"
+                    src={SkillsIcon}
+                    alt="skill"
+                  />
+                </button>
+              </div>
+            </div>
+            <div className="flex items-end justify-end gap-4 animate-appear">
+              <Button
+                handleButtonClick={handleProject}
+                buttonLabel={homepageData.projectsButtonLabel}
+              />
+              <div className="flex flex-col items-center">
+                <GradientBar />
+                <button
+                  onClick={handleProject}
+                  className="w-8 h-8 p-1 bg-linear-blue rounded-full flex items-center justify-center"
+                >
+                  <img
+                    className="h-full w-full"
+                    src={ProjectsIcon}
+                    alt="projects"
+                  />
+                </button>
+              </div>
+            </div>
+            <div className="flex items-center justify-end gap-4 animate-appear">
+              <Button
+                handleButtonClick={handleContact}
+                buttonLabel={homepageData.contanctButtonLabel}
+              />
+              <div className="flex flex-col items-center">
+                <GradientBar />
+                <button
+                  onClick={handleContact}
+                  className="w-8 h-8 p-1 bg-linear-blue rounded-full flex items-center justify-center"
+                >
+                  <img
+                    className="h-full w-full"
+                    src={ContactIcon}
+                    alt="contact"
+                  />
+                </button>
+                <GradientBar />
+              </div>
+            </div>
+            <div className="flex gap-4">
+              {" "}
+              <div className="w-8 h-8 hover:scale-150 hover:cursor-pointer p-1 bg-linear-blue rounded-full flex items-center justify-center">
+                <a href="https://github.com/marymetilda">
+                  <img className="h-full w-full" src={GithubIcon} alt="about" />
+                </a>
+              </div>
+              <div className="w-8 h-8 hover:scale-150 hover:cursor-pointer p-1 bg-linear-blue rounded-full flex items-center justify-center">
+                <a href="https://drive.google.com/file/d/1l5i1ToE3xU7X3W9k6YBdK9_IVTvLNRTN/view?usp=drivesdk">
+                  <img className="h-full w-full" src={ResumeIcon} alt="about" />
+                </a>
+              </div>
+              <div className="w-8 h-8 hover:scale-150 hover:cursor-pointer p-1 bg-linear-blue rounded-full flex items-center justify-center">
+                <a href="https://www.linkedin.com/in/mary-metilda-9961aa279">
+                  <img className="h-full w-full" src={LinkedIn} alt="about" />
+                </a>
+              </div>
+            </div>
           </div>
         </div>
-      ) : (
-        <div className="flex flex-col items-center justify-start h-full w-full animate-appear">
-          <div className="w-[40rem] h-20 relative mt-4">
-            <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
-            <SparklesCore
-              id="particle-2"
-              background="transparent"
-              minSize={0.4}
-              maxSize={1}
-              particleDensity={1200}
-              className="w-full h-full"
-              particleColor="#FFFFFF"
-            />
-            <div className="absolute inset-x-20 bottom-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
-            <div className="absolute inset-x-20 bottom-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
-            <div className="absolute inset-x-60 bottom-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
-            <div className="absolute inset-x-60 bottom-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <br />
-            <div className="text-[3vw] md:text-xl text-sky-600 font-semibold h-fit flex justify-start items-center w-full">
-              <p> I am </p>
-              <FlipWords words={words} />
-            </div>
+        <div className="w-[40rem] h-20 relative mt-4">
+          {/* Gradients */}
+          <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+          <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+          <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+          <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
 
-            <div className="flex flex-col items-end md:gap-0 pr-0 md:pr-[4vw]">
-              <div className="flex items-end justify-end gap-4 animate-appear pt-8">
-                <Button
-                  handleButtonClick={handleAbout}
-                  buttonLabel={homepageData.aboutButtonLabel}
-                />
-                <div className="flex flex-col items-center justify-between">
-                  <button
-                    onClick={handleAbout}
-                    className="w-8 h-8 p-1 bg-linear-blue rounded-full flex items-center justify-center"
-                  >
-                    <img
-                      className="h-full w-full"
-                      src={AboutButton}
-                      alt="about"
-                    />
-                  </button>
-                </div>
-              </div>
-              <div className="flex items-end justify-end gap-4 animate-appear">
-                <Button
-                  handleButtonClick={handleSkill}
-                  buttonLabel={homepageData.skillsButtonLabel}
-                />
-                <div className="flex flex-col items-center">
-                  <GradientBar />
-                  <button
-                    onClick={handleSkill}
-                    className="w-8 h-8 p-1 bg-linear-blue rounded-full flex items-center justify-center"
-                  >
-                    <img
-                      className="h-full w-full rounded-full"
-                      src={SkillsIcon}
-                      alt="skill"
-                    />
-                  </button>
-                </div>
-              </div>
-              <div className="flex items-end justify-end gap-4 animate-appear">
-                <Button
-                  handleButtonClick={handleProject}
-                  buttonLabel={homepageData.projectsButtonLabel}
-                />
-                <div className="flex flex-col items-center">
-                  <GradientBar />
-                  <button
-                    onClick={handleProject}
-                    className="w-8 h-8 p-1 bg-linear-blue rounded-full flex items-center justify-center"
-                  >
-                    <img
-                      className="h-full w-full"
-                      src={ProjectsIcon}
-                      alt="projects"
-                    />
-                  </button>
-                </div>
-              </div>
-              <div className="flex items-center justify-end gap-4 animate-appear">
-                <Button
-                  handleButtonClick={handleContact}
-                  buttonLabel={homepageData.contanctButtonLabel}
-                />
-                <div className="flex flex-col items-center">
-                  <GradientBar />
-                  <button
-                    onClick={handleContact}
-                    className="w-8 h-8 p-1 bg-linear-blue rounded-full flex items-center justify-center"
-                  >
-                    <img
-                      className="h-full w-full"
-                      src={ContactIcon}
-                      alt="contact"
-                    />
-                  </button>
-                  <GradientBar />
-                </div>
-              </div>
-              <div className="flex gap-4">
-                {" "}
-                <div className="w-8 h-8 hover:scale-150 hover:cursor-pointer p-1 bg-linear-blue rounded-full flex items-center justify-center">
-                  <a href="https://github.com/marymetilda">
-                    <img
-                      className="h-full w-full"
-                      src={GithubIcon}
-                      alt="about"
-                    />
-                  </a>
-                </div>
-                <div className="w-8 h-8 hover:scale-150 hover:cursor-pointer p-1 bg-linear-blue rounded-full flex items-center justify-center">
-                  <a href="https://drive.google.com/file/d/1l5i1ToE3xU7X3W9k6YBdK9_IVTvLNRTN/view?usp=drivesdk">
-                    <img
-                      className="h-full w-full"
-                      src={ResumeIcon}
-                      alt="about"
-                    />
-                  </a>
-                </div>
-                <div className="w-8 h-8 hover:scale-150 hover:cursor-pointer p-1 bg-linear-blue rounded-full flex items-center justify-center">
-                  <a href="https://www.linkedin.com/in/mary-metilda-9961aa279">
-                    <img className="h-full w-full" src={LinkedIn} alt="about" />
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="w-[40rem] h-20 relative mt-4">
-            {/* Gradients */}
-            <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
-            <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
-            <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
-            <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+          {/* Core component */}
+          <SparklesCore
+            id="particle-3"
+            background="transparent"
+            minSize={0.4}
+            maxSize={1}
+            particleDensity={1200}
+            className="w-full h-full"
+            particleColor="#FFFFFF"
+          />
 
-            {/* Core component */}
-            <SparklesCore
-              id="particle-3"
-              background="transparent"
-              minSize={0.4}
-              maxSize={1}
-              particleDensity={1200}
-              className="w-full h-full"
-              particleColor="#FFFFFF"
-            />
-
-            {/* Radial Gradient to prevent sharp edges */}
-            <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
-          </div>
+          {/* Radial Gradient to prevent sharp edges */}
+          <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
         </div>
-      )}
+      </div>
     </div>
   );
 }
